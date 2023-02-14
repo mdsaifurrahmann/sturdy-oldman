61b0a307b35f0154992dcad0a13e0f005ba4a784
mdsaifurrahmann <md.saifurrahmann029@gmail.com>
2023-02-15 02:35:33 +0600

    add slider with db & seeder done

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,84 +3,84 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\File;
 
-class HomeController extends Controller
+
+class HomeSliderController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
+     * @throws ValidationException
      */
-    public function index()
+    public function create(Request $request)
     {
-        //
+        $slider = $request->slider;
+        $data = [];
+        foreach ($slider as $key => $value) {
+            if ($file = $request->hasFile('slider.' . $key . '.image')) {
+
+                $image = 'slider.' . $key . '.image';
+                $title = 'slider.' . $key . '.title';
+                $desc = 'slider.' . $key . '.desc';
+
+                $request->validate([
+                    $image => 'required|image|mimes:jpeg,png,jpg|max:1024',
+                    $title => 'required|string',
+                    $desc => 'required|string',
+                ]);
+                $file = $request->file('slider.' . $key . '.image');
+                $fileName = time() . Str::random(16) . '-slider-' . $key . '.' . $file->extension();
+                $destinationPath = public_path() . '/images/slider';
+                $file->move($destinationPath, $fileName);
+                $slider[$key]['image'] = $fileName;
+
+                $info = [
+                    'title' => $request->input($title),
+                    'desc' => $request->input($desc),
+                    'image' => $fileName,
+                ];
+
+                array_push($data, $info);
+            }
+        }
+
+        $retriveData = json_decode(DB::table('home_data')->where('target', 'slider')->value('data'));
+
+        $final = array_merge($retriveData, $data);
+
+        DB::table('home_data')->update([
+            'target' => 'slider',
+            'data' => json_encode($final),
+        ]);
+
+        return redirect()->back()->with('success', 'Slider Added Successfully');
     }
 
-    /**
-     * Show Home Page
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    public function sliderList()
     {
-        $hello = 'Hello World';
-        $pageConfigs = ['blankPage' => true];
-        return view('/frontend/home', ['pageConfigs' => $pageConfigs, 'hello' => $hello]);
+        $data = json_decode(DB::table('home_data')->where('target', 'slider')->value('data'));
+        return view('frontend.home', compact('data'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
-        //
+        $data = json_decode(DB::table('home_data')->where('target', 'slider')->value('data'));
+
+        $reinsert = array();
+
+        foreach ($data as $key => $value) {
+            // remove item from array
+            if ($key == $id) {
+                \File::exists(public_path('/images/slider/' . $value->image)) ? File::delete(public_path('/images/slider/' . $value->image)) : null;
+            } else {
+                array_push($reinsert, $value);
+            }
+        }
+
+        DB::table('home_data')->where('target', 'slider')->update([
+            'data' => json_encode($reinsert),
+        ]);
     }
 }
