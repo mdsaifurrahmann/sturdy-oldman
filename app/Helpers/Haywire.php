514d50022090b06ce925545bf793a671cf409284
mdsaifurrahmann <md.saifurrahmann029@gmail.com>
2023-09-02 22:30:43 +0600

    Haywire Updated

--- app/Helpers/Haywire.php
+++ app/Helpers/Haywire.php
@@ -6,7 +6,6 @@ use Config;
 use Illuminate\Support\Str;
 use GuzzleHttp\Client;
 use Illuminate\Support\Facades\Mail;
-use Illuminate\Support\Facades\Crypt;
 
 class Haywire
 {
@@ -32,7 +31,7 @@ class Haywire
         $pingCommand = sprintf('ping -c 1 %s', escapeshellarg($serverHost));
         $pingResult = shell_exec($pingCommand);
 
-        if (strpos($pingResult, '1 packets received') !== false) {
+        if (strpos($pingResult, '1 received') !== false || strpos($pingResult, '1 packets received') !== false) {
             try {
                 $response = $client->post('https://v1.codebumble.net/v1/license/validate', [
                     'form_params' => [
@@ -45,7 +44,7 @@ class Haywire
                 if ($getResponse['status'] != 'valid') {
 
                     Mail::send('email.solidity', ['interface' => $interface, 'modifiedFiles' => ''], function ($message) {
-                        $message->to('md.saifurrahmann029@gmail.com')
+                        $message->to('integrity@codebumble.net')
                             ->subject('Indispose Detected on app ' . config('app.name'));
                     });
                     return $getResponse['status'];
